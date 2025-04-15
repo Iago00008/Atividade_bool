9f569d9a701e756ad38db5f83414780793df17af
Iago00008 <iagofazevedo.08@gmail.com>
2025-04-15 09:47:40 -0300

    02

---
+++ Atividade-02]/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Digite um número:");
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        bool resultado = num % 2 == 0;
+
+
+        if (resultado)
+        {
+            Console.WriteLine("O número é par.");
+        }
+
+    }
+}
